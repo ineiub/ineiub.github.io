6172c59dc056f1da144d5a4e8cbcff7324247fec
Massimo Bernini <ineiub@gmail.com>
2025-03-13 17:00:11 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -103,3 +103,21 @@ if (localStorage.getItem("theme") === "light_theme") {
   document.body.classList.remove("light_theme");
   document.body.classList.add("dark_theme");
 }
+
+/**
+ * contact form submission
+ */
+const contactForm = document.querySelector(".contact-form");
+
+contactForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const message = document.getElementById("message").value;
+
+  const mailtoLink = `mailto:ineiub@gmail.com?subject=New Contact Form Submission&body=Name: ${name}%0D%0AEmail: ${email}%0D%0APhone: ${phone}%0D%0AMessage: ${message}`;
+
+  window.location.href = mailtoLink;
+});
